7d5d7906d0c91db110993bcba02276aedf89921f
sasank <vnssasank@gmail.com>
2025-05-15 19:00:00 +0530

    Codes.cs

---
+++ May 15th/Class/Employee.cs
@@ -0,0 +1,10 @@
+namespace WebApplication4.Models
+{
+    public class Employee
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string department { get; set; }
+    }
+}
